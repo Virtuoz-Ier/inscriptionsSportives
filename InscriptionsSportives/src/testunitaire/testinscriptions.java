d54dd67cffca2c853eb5fbc9a70f9733c10bb242
Maha Amar <amarmaha2707@gmail.com>
2017-12-02 01:43:21 +0100

    suite test unitaire

--- InscriptionsSportives/src/testunitaire/testinscriptions.java
+++ InscriptionsSportives/src/testunitaire/testinscriptions.java
@@ -1,5 +1,78 @@
 package testunitaire;
 
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import org.junit.Test;
+
+import inscriptions.*;
 public class testinscriptions {
+
+	Inscriptions inscriptionTest = Inscriptions.getInscriptions();
+	Competition competitionEquipeTest = inscriptionTest.createCompetition("CompetEquipeTest",  null, true);
+	Competition competitionSoloTest = inscriptionTest.createCompetition("CompetSoloTest",  null, false);
+	Equipe equipeTest = inscriptionTest.createEquipe("EquipeTest");
+	Personne personneTest = inscriptionTest.createPersonne("TEST", "test", "stest@gmail.com");
+	
+	@Test
+	public void testGetCompetitions() {
+		Set<Competition> setCompetitionTest = inscriptionTest.getCompetitions();
+		assertFalse(setCompetitionTest.isEmpty());
+		
+	}
+
+	@Test
+	public void testGetCandidats() {
+		Set<Candidat> setCandidatTest = inscriptionTest.getCandidats();
+		assertFalse(setCandidatTest.isEmpty());
+	}
+
+	@Test
+	public void testCreateCompetition() {
+		Competition competitionTest2 = inscriptionTest.createCompetition("Test",  null, false);
+		Set <Competition> setCompetitionTest = inscriptionTest.getCompetitions();
+		assertTrue(setCompetitionTest.contains(competitionTest2));
+	}
+
+	@Test
+	public void testCreatePersonne() {
+		Personne personneTest2 = inscriptionTest.createPersonne("", "", "");
+		Set<Candidat>setPersonneTest2 = inscriptionTest.getCandidats();
+		assertTrue(setPersonneTest2.contains(personneTest2));
+	}
+
+	@Test
+	public void testCreateEquipe() {
+		Equipe equipeTest2 = inscriptionTest.createEquipe("");
+		Set<Candidat>setEquipeTest2 = inscriptionTest.getCandidats();
+		assertTrue(setEquipeTest2.contains(equipeTest2));
+	}
+
+	@Test
+	public void testRemoveCompetition() {
+		Set<Competition> setCompetitionTest = inscriptionTest.getCompetitions();
+		competitionSoloTest.delete();
+		assertFalse(setCompetitionTest.contains(competitionSoloTest));
+		
+	}
+
+	@Test
+	public void testRemoveCandidat() {
+		Set<Candidat>setCandidatTest = inscriptionTest.getCandidats();
+		personneTest.delete();
+		assertFalse(setCandidatTest.contains(personneTest));
+	}
+
+	@Test
+	public void testGetInscriptions() {
+		assertEquals(inscriptionTest, Inscriptions.getInscriptions());
+	}
+
+	@Test
+	public void testSauvegarder() {
+		fail("...");
+	}
 
 }
--- InscriptionsSportives/src/testunitaire/testpersonne.java
+++ InscriptionsSportives/src/testunitaire/testpersonne.java
@@ -1,5 +1,56 @@
 package testunitaire;
 
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.Test;
+import inscriptions.*;
+
 public class testpersonne {
+	
+	Inscriptions inscriptionTest = Inscriptions.getInscriptions();
+	Personne personneTest = inscriptionTest.createPersonne ("TEST", "test", "tTEST@gmail.com");
+	
+	
+	@Test
+	public void testDelete() {
+		Equipe equipeTest = inscriptionTest.createEquipe("EQUIPE TEST");
+		equipeTest.add(personneTest);
+		Set<Equipe> setEquipesTest = personneTest.getEquipes();
+		personneTest.delete();
+		assertFalse(setEquipesTest.contains(personneTest));
+	}
+
+	@Test
+	public void testGetPrenom() {
+		assertEquals("test", personneTest.getPrenom());
+	}
+
+	@Test
+	public void testSetPrenom() {
+		personneTest.setPrenom("test1");
+		assertEquals("test1", personneTest.getPrenom());
+	}
+
+	@Test
+	public void testGetMail() {
+		assertEquals("tTEST@gmail.com", personneTest.getMail());
+	}
+
+	@Test
+	public void testSetMail() {
+		personneTest.setMail("TEST@gmail.com");
+		assertEquals("TEST@gmail.com", personneTest.getMail());
+	}
+
+	@Test
+	public void testGetEquipes() {
+		Equipe equipeTest = inscriptionTest.createEquipe("EQUIPE TEST");
+		equipeTest.add(personneTest);
+		Set<Equipe> setEquipesTest = personneTest.getEquipes();
+		assertTrue(setEquipesTest.contains(equipeTest));
+	}
 
 }
